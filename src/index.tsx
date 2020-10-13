dae90a478aff4dd443757f8bf895ab87f102f2b6
Daniil <daniil.petro3@gmail.com>
2020-10-13 19:23:45 +0500

    create folder structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
+import './scss/index.scss';
 import { App } from './components/App';
 
 ReactDOM.render(
   <App userName="Daniil" projectName="Six cities - custom SPA" />,
-  document.querySelector('.root'),
+  document.querySelector('#root'),
 );
