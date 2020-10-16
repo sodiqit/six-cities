5f7b33044d7ed4e4160c7f85340b678fea19343c
Daniil <daniil.petro3@gmail.com>
2020-10-16 23:25:31 +0500

    temporary mock fn and rooms

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './scss/index.scss';
-import { App } from './components/App';
+import './img/logo.svg';
+import './img/apartment-01.jpg';
+import './img/room.jpg';
+import './img/apartment-02.jpg';
+import './img/apartment-03.jpg';
+import { App } from './components/App/App';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+const rooms = [
+  'Beautiful & luxurious apartment at great location',
+  'Wood and stone place',
+  'Canal View Prinsengracht',
+  'Nice, cozy, warm big bed apartment',
+];
+
+const fn = () => {
+  console.log('clicked');
+};
+
+ReactDOM.render(<App onClick={fn} rooms={rooms} />, document.querySelector('#root'));
