7cb1db899d53ecdbb8b71d76adffb7d0b5b30514
Daniil <daniil.petro3@gmail.com>
2020-10-16 22:45:03 +0500

    render main static page

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,4 @@ import ReactDOM from 'react-dom';
 import './scss/index.scss';
 import { App } from './components/App';
 
-ReactDOM.render(
-  <App userName="Daniil" projectName="Six cities - custom SPA" />,
-  document.querySelector('#root'),
-);
+ReactDOM.render(<App />, document.querySelector('#root'));
