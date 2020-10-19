80f1c9df26ef93656682b9d34f4bd18a3a5748ea
Daniil <daniil.petro3@gmail.com>
2020-10-19 23:07:05 +0500

    minor refactor

--- src/index.tsx
+++ src/index.tsx
@@ -6,17 +6,7 @@ import './img/apartment-01.jpg';
 import './img/room.jpg';
 import './img/apartment-02.jpg';
 import './img/apartment-03.jpg';
+import { offers } from './mocks/offers';
 import { App } from './components/App/App';
 
-const rooms = [
-  'Beautiful & luxurious apartment at great location',
-  'Wood and stone place',
-  'Canal View Prinsengracht',
-  'Nice, cozy, warm big bed apartment',
-];
-
-const fn = () => {
-  console.log('clicked');
-};
-
-ReactDOM.render(<App onClick={fn} rooms={rooms} />, document.querySelector('#root'));
+ReactDOM.render(<App rooms={offers} />, document.querySelector('#root'));
