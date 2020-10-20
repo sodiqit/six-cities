c4f3d739b5ed4761db916d0bb8aff9efb6e8263f
Daniil <daniil.petro3@gmail.com>
2020-10-20 11:43:52 +0500

    write test for room card component

---
+++ src/components/Room-card/Room-card.e2e.test.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { RoomCard } from './Room-card';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('Room card', () => {
+  it('correctly hover', () => {
+    const fn = jest.fn();
+    const mockRoom = {
+      id: '0',
+      city: 'amsterdam',
+      title: 'Beautiful & luxurious apartment at great location',
+      price: 120,
+      rating: 4,
+      typeRoom: 'Apartment',
+      img: 'img/apartment-01.jpg',
+      isPremium: true,
+      isBookmark: false,
+    };
+    const roomCard = shallow(<RoomCard room={mockRoom} onMouseEnter={fn} />);
+
+    roomCard.simulate('mouseenter');
+
+    expect(fn).toHaveBeenCalled();
+    expect(fn).toHaveBeenCalledTimes(1);
+  });
+});
---
+++ src/components/Room-card/Room-card.test.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { RoomCard } from './Room-card';
+
+describe('Room card', () => {
+  it('correctly render', () => {
+    const fn = jest.fn();
+    const mockRoom = {
+      id: '0',
+      city: 'amsterdam',
+      title: 'Beautiful & luxurious apartment at great location',
+      price: 120,
+      rating: 4,
+      typeRoom: 'Apartment',
+      img: 'img/apartment-01.jpg',
+      isPremium: true,
+      isBookmark: false,
+    };
+    const tree = renderer.create(<RoomCard room={mockRoom} onMouseEnter={fn} />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
