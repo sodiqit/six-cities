29ad6ddde52069a2c27e5969ae4bf2fc3d29b3d0
Daniil <daniil.petro3@gmail.com>
2020-10-27 15:01:54 +0500

    add new alias

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,6 @@ module.exports = {
     "^components(.*)": "<rootDir>/src/components$1",
     "^mocks(.*)": "<rootDir>/src/mocks/$1",
     "^utils(.*)": "<rootDir>/src/utils/$1",
+    "^redux(.*)": "<rootDir>/src/redux/$1",
   }
 };
