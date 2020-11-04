ab2bfadb0ec04e3d2d4d717ca91770a570e719c1
Daniil <daniil.petro3@gmail.com>
2020-11-04 15:25:35 +0500

    add theme and path

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -3,18 +3,20 @@ import Header from 'components/Header/Header';
 
 interface MainLayoutProps {
   title: string;
+  theme?: string;
+  path?: string;
 }
 
 const MainLayout: FC<MainLayoutProps> = (props) => {
-  const { title, children } = props;
+  const { title, theme = '', path = './', children } = props;
 
   useEffect(() => {
     document.title = `${title} | Six cities`;
   }, [title]);
 
   return (
-    <div className="page page--gray page--main">
-      <Header />
+    <div className={`page ${theme === 'main' ? 'page--gray page--main' : ''}`}>
+      <Header path={path} />
       {children}
     </div>
   );
